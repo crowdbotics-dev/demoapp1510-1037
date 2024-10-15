19f6319170cff15eb85fbc3663fbc0d5f5529812
Crowdbotics <engineering@crowdbotics.com>
2024-10-15 05:19:45 +0000

    Initial commit

---
+++ backend/demoapp1510_1037/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for demoapp1510_1037 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "demoapp1510_1037.settings"
+)
+
+application = get_wsgi_application()
